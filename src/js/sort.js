b23b9e4fbc8e08a5a5ff471dbde4bc22a478b7ff
paopaojun <1393212597@qq.com>
2016-12-28 20:30:14 +0800

    更新个页面链接

--- src/js/sort.js
+++ src/js/sort.js
@@ -3,20 +3,39 @@
  */
 $(function () {
     $.ajax({
-        url: '../json/sort.json',
+        url: '../json/sortphone.json',
         type: 'get',
         dataType: 'json',
         // contentType: 'application/json',
         success: function (data) {
-            sortShop(data);
+            sortPhone(data);
+            console.log(data);
+        }
+    });
+
+    $.ajax({
+        url: '../json/sortpc.json',
+        type: 'get',
+        dataType: 'json',
+        // contentType: 'application/json',
+        success: function (data) {
+            sortpc(data);
             console.log(data);
         }
     });
 });
-function sortShop(ajaxdata) {
+function sortPhone(ajaxdata) {
     var template='';
     for(var i=0;i<ajaxdata.data.length;i++){
-        template=template+'<div class="shopPhone"><img src="'+ajaxdata.data[i]['src']+'"/><p class="words">'+ajaxdata.data[i]['words']+'</p></div>';
+        template=template+'<div class="shop"><img src="'+ajaxdata.data[i]['src']+'"/><p class="words">'+ajaxdata.data[i]['words']+'</p></div>';
     }
     $('.phone').append(template);
+}
+
+function sortpc(ajaxdata) {
+    var template='';
+    for(var i=0;i<ajaxdata.data.length;i++){
+        template=template+'<div class="shop"><img src="'+ajaxdata.data[i]['src']+'"/><p class="words">'+ajaxdata.data[i]['words']+'</p></div>';
+    }
+    $('.pad').append(template);
 }
